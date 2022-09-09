615713d2fa08e6aab1af15f164202179af9c434d
Chris Müller <christian.mueller23@googlemail.com>
2022-09-09 18:34:27 +0200

    [FEATURE] Continue calculation after equal

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,20 +1,20 @@
 let numberBtn = document.querySelector(".numbers"),
   clearBtn = document.querySelector("#clear"),
   operatorBtns = document.querySelector(".operators"),
-  result = "",
-  equal = document.querySelector("#equal"),
-  digitOne,
-  digitTwo;
+  equal = document.querySelector("#equal");
+  
 
 // Object with data for calculation
 var calculationValue = {
   numberOne: [],
   numberTwo: [],
+  result: [],
   operator: null,
   reset: function () {
     this.numberOne = [];
     this.numberTwo = [];
     this.operator = null;
+    this.result = [];
   },
 };
 
@@ -22,18 +22,57 @@ var calculationValue = {
 var outputField = document.getElementById("outputField");
 outputField.innerHTML = 0;
 
+// Shows the digit array in outputfield
+function showDigit(sign) {
+  outputField.innerHTML = sign;
+}
+
+// Fill the calculationValue object with value
+function updateData(key, value) {
+  key.push(value);
+  showDigit(key.join(""));
+}
+
+// Calculate both value of numberOne & numberTwo
+function calculate(digitOne, digitTwo) {
+  if (calculationValue.operator === "+") {
+    // Unary plus operator to convert them to numbers first
+    calculationValue.result = +digitOne + +digitTwo;
+  } else if (calculationValue.operator === "-") {
+    calculationValue.result = digitOne - digitTwo;
+  } else if (calculationValue.operator === "*") {
+    calculationValue.result = digitOne * digitTwo;
+  } else if (calculationValue.operator === "/") {
+    calculationValue.result = digitOne / digitTwo;
+  } else if (calculationValue.operator === "%") {
+    calculationValue.result = digitOne %= digitTwo;
+  }
+}
+
+// Reset numberOne and operator to continue the calculation
+function continueCalc() {
+  calculationValue.numberOne = [];
+  calculationValue.numberTwo = [];
+  calculationValue.operator = null;
+}
+
+function fixedDecimal(result) {
+  // If decimal number more than 4 decimal places
+  if (result.toString().indexOf(".") !== -1) {
+    result = result.toFixed(4);
+    console.log(result);
+  }
+}
+
 // Select all Buttons and add value to object
 document.querySelectorAll(".numbers").forEach((numberBtn) => {
   numberBtn.addEventListener("click", () => {
     if (calculationValue.operator !== null) {
-      calculationValue.numberTwo.push(numberBtn.value);
-      digitTwo = calculationValue.numberTwo.join("");
-      outputField.innerHTML = calculationValue.numberTwo;
-      showDigit(digitTwo);
+      updateData(calculationValue.numberTwo, numberBtn.value);
+      console.log(calculationValue.numberTwo);
     } else {
-      calculationValue.numberOne.push(numberBtn.value);
-      digitOne = calculationValue.numberOne.join("");
-      showDigit(digitOne);
+      updateData(calculationValue.numberOne, numberBtn.value);
+      console.log(calculationValue.numberOne);
     }
   });
 });
@@ -41,9 +80,18 @@ document.querySelectorAll(".numbers").forEach((numberBtn) => {
 // Press operator button
 document.querySelectorAll(".operators").forEach((operator) => {
   operator.addEventListener("click", () => {
-    if (calculationValue.numberOne !== []) {
+    if (outputField.length === 0) {
+      alert("Gib eine Zahl ein!");
+    } else if (calculationValue.result.length > 0) {
+      continueCalc();
       calculationValue.operator = operator.value;
       showDigit(calculationValue.operator);
+      calculate(calculationValue.numberTwo, calculationValue.result);
+    } else {
+      outputField.classList.remove("upperOutput");
+      calculationValue.operator = operator.value;
+      showDigit(calculationValue.operator);
+      console.log(calculationValue);
     }
   });
 });
@@ -53,35 +101,20 @@ clearBtn.addEventListener("click", () => {
   calculationValue.reset();
   outputField.classList.remove("upperOutput");
   outputField.innerHTML = 0;
-  console.log(calculationValue);
 });
 
-// Shows the digit array in outputfield
-function showDigit(calculation) {
-  outputField.innerHTML = calculation;
-}
-
-// Show result
+// Show result or continue calculation
 equal.addEventListener("click", () => {
-  if (calculationValue.operator === "+") {
-    // Unary plus operator to convert them to numbers first
-    result = +digitOne + +digitTwo;
-  } else if (calculationValue.operator === "-") {
-    result = digitOne - digitTwo;
-  } else if (calculationValue.operator === "*") {
-    result = digitOne * digitTwo;
-  } else if (calculationValue.operator === "/") {
-    result = digitOne / digitTwo;
-  } else if (calculationValue.operator === "%") {
-    result = (digitOne %= digitTwo);
+  if (calculationValue.numberTwo.length > 0 && calculationValue.result > 0) {
+    calculate(calculationValue.numberTwo, calculationValue.result);
+  } else {
+    calculate(calculationValue.numberOne, calculationValue.numberTwo);
   }
-  calculationValue.reset();
   outputField.className = "upperOutput";
-  // If decimal number more than 4 decimal places 
-  if (result.toString().indexOf('.') !== -1) {
-    result = result.toFixed(4);
-  }
-  outputField.innerHTML = result;
+  fixedDecimal(calculationValue.result);
+  outputField.innerHTML = calculationValue.result;
+  continueCalc();
+  console.log(calculationValue.result);
 });
 
 // Keydown event
